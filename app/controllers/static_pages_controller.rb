533d1b571db23e6caecf3bdaaf14f542a5326811
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2016-10-16 14:24:09 -0700

    added terms of service and privacy policy

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,9 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-  def about
+  def tos
+  end
+
+  def privacy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get 'static_pages/home'
-  get '/about', to: 'static_pages#about'
   get '/signup',  to: 'users#new'
+  get '/tos', to: 'static_pages#tos'
+  get '/privacy', to: 'static_pages#privacy'
 
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
