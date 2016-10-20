99cfe990bba882b1f200a60005592fffa5237313
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2016-10-20 01:00:11 -0700

    added columns to groups and memberships

---
+++ db/migrate/20161020075649_add_group_id_to_memberships.rb
@@ -0,0 +1,5 @@
+class AddGroupIdToMemberships < ActiveRecord::Migration[5.0]
+  def change
+    add_column :memberships, :group_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019223237) do
+ActiveRecord::Schema.define(version: 20161020075649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,11 +18,15 @@ ActiveRecord::Schema.define(version: 20161019223237) do
   create_table "groups", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "location"
   end
 
   create_table "memberships", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "group_id"
   end
 
   create_table "users", force: :cascade do |t|
