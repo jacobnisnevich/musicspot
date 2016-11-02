fb3bbe72e7c1a28510965b3f4014ff6426d12b70
DylanFlanders <dylan_flanders@yahoo.com>
2016-11-02 01:09:38 -0700

    Change tabs to spaces

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -2,31 +2,31 @@ require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
 
-	def setup
-	  @event = Event.new(
-		name: "Band Concert",
-		location: "90024",
-		start_datetime: DateTime.new(2016,10,30,15),
-		end_datetime: DateTime.new(2016,10,30,16),
-		description: "An awesome concert.")
-	end
+  def setup
+    @event = Event.new(
+      name: "Band Concert",
+      location: "90024",
+      start_datetime: DateTime.new(2016,10,30,15),
+      end_datetime: DateTime.new(2016,10,30,16),
+      description: "An awesome concert.")
+  end
 
-	test "valid event" do
-	  assert @event.valid?
-	end
+  test "valid event" do
+    assert @event.valid?
+  end
 
-	test "event name should be present" do
-	  @event.name = ""
-	  assert_not @event.valid?
-	end
+  test "event name should be present" do
+    @event.name = ""
+    assert_not @event.valid?
+  end
 
-	test "event name should not be too long" do
-	  @event.name = "a" * 51
-	  assert_not @event.valid?
-	end
+  test "event name should not be too long" do
+    @event.name = "a" * 51
+    assert_not @event.valid?
+  end
 
-	test "event location should not be too long" do
-	  @event.location = "1" * 51
-	  assert_not @event.valid?
-	end
+  test "event location should not be too long" do
+    @event.location = "1" * 51
+    assert_not @event.valid?
+  end
 end
