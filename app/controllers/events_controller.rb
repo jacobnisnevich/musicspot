929112016bdc656e29a959b4f2dc79340e72cb19
DylanFlanders <dylan_flanders@yahoo.com>
2016-11-02 01:05:13 -0700

    Validate new event submissions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,16 +9,24 @@ class EventsController < ApplicationController
   end
 
   def submit
-    @event = Event.new(params.require(:event).permit(:end_datetime, :name, :location, :description, :start_datetime))
+    @event = Event.new(event_params)
     @event.groups << Group.find_by(@group_id)
 
-    @event.save
-
-    redirect_to '/events'
+    if (@event.save)
+      redirect_to '/events'
+    else
+      render 'new'
+    end
   end
 
   def show
     @full_width = true
     @event = Event.find_by(id: params[:id])
   end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:end_datetime, :name, :location, :description, :start_datetime)
+    end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
 	has_many :group_events
 	has_many :groups, :through => :group_events
+
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :location, presence: true, length: { maximum: 50 }
+	validates :start_datetime, presence: true
 end
