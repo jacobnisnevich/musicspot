76c9d9f6bae96333d297bc88d4c31e7d9600cfdf
Jason Yang <jasonyang96@ucla.edu>
2016-11-09 11:16:43 -0800

    added fields to schema and form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103221034) do
+ActiveRecord::Schema.define(version: 20161109190359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,13 +56,16 @@ ActiveRecord::Schema.define(version: 20161103221034) do
   end
 
   create_table "groups", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.string   "name"
     t.string   "location"
     t.string   "description"
     t.string   "group_type"
     t.text     "about"
+    t.string   "image_url"
+    t.string   "youtube_url"
+    t.string   "soundcloud_url"
   end
 
   create_table "memberships", force: :cascade do |t|
