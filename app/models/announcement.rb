e788d79859e5f07d918f5c5401527e133e61aa16
DylanFlanders <dylan_flanders@yahoo.com>
2016-11-20 16:20:12 -0800

    Add validations for user profile, and announcement title length. Issue #7

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,6 +1,6 @@
 class Announcement < ApplicationRecord
 	belongs_to :group
 
-	validates :title, presence: true
+	validates :title, presence: true, length: { maximum: 50 }
 	validates :description, presence: true
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,9 @@
 class Profile < ApplicationRecord
 	belongs_to :user
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 200 }, format: { with: VALID_EMAIL_REGEX }
+  validates :musical_role, presence: true, length: { maximum: 50 }
+  validates :interests, length: { maximum: 100 }
+  validates :other, length: { maximum: 100 }
 end
