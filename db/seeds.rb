b2dde106ae4012f040cebb24f128d403eaf449ca
DylanFlanders <dylan_flanders@yahoo.com>
2016-11-22 21:29:09 -0800

    Add fake seed data for groups

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,43 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Event.create!(
+group = Group.create!(
+  name: "Awechords",
+  location: "90024",
+  description: "A co-ed cappella group at UCLA",
+  group_type: "Acapella",
+  about: "We are a co-ed cappella group at UCLA that strives to share our passion for music with the LA community and empower our members' musicality."
+)
+event = Event.create!(
   name: "Concert",
   location: "90024",
   start_datetime: DateTime.new(2016,11,1,15),
   end_datetime: DateTime.new(2016,11,1,16),
   description: "An awesome concert!",
-  image: "http://www.clipartbest.com/cliparts/4T9/bKg/4T9bKgLTE.png"
+  image: "http://www.clipartbest.com/cliparts/4T9/bKg/4T9bKgLTE.png",
 )
+event.groups << group
 
 99.times do |n|
-  name  = "#{Faker::Company.name} Concert"
-  location = Faker::Address.zip_code
-  description = Faker::Hipster.sentence
-  Event.create!(name:  name,
-    location: location,
+  name  = Faker::Company.name
+
+  group = Group.create!(
+    name: name,
+    location: Faker::Address.zip_code,
+    description: Faker::Hipster.sentence,
+    group_type: Faker::Music.instrument,
+    about: Faker::Hipster.paragraph
+  )
+  event = Event.create!(
+    name:  "#{name} Concert",
+    location: Faker::Address.zip_code,
     start_datetime: DateTime.new(2016,11,1,15),
     end_datetime: DateTime.new(2016,11,1,16),
-    description: description,
-    image: "http://www.clipartbest.com/cliparts/4T9/bKg/4T9bKgLTE.png")
+    description: Faker::Hipster.sentence,
+    image: "http://www.clipartbest.com/cliparts/4T9/bKg/4T9bKgLTE.png"
+  )
+  event.groups << group
 end
 
+puts "Created #{Group.count} events"
 puts "Created #{Event.count} events"
